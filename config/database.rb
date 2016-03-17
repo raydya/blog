a2dc8c278533cbcabfe257140459f43d1ddb9952
raydya <dya.ilaempire@gmail.com>
2016-03-17 12:02:02 +0800

    update database

--- config/database.rb
+++ config/database.rb
@@ -30,11 +30,11 @@ DataMapper.setup(:default, YAML.load_file(Padrino.root('config/database.yml'))[R
 #   database: example_production
 #
 
-DataMapper.logger = logger
-DataMapper::Property::String.length(255)
-
-case Padrino.env
-  when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_development.db"))
-  when :production  then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_production.db"))
-  when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_test.db"))
-end
+#DataMapper.logger = logger
+#DataMapper::Property::String.length(255)
+#
+#case Padrino.env
+#  when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_development.db"))
+#  when :production  then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_production.db"))
+#  when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_test.db"))
+#end
